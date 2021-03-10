02d1b198edb85d066167de0ff2b3d891f51a50ed
Сунцов Владимир <vovka.steel911@mail.ru>
2021-03-10 22:26:14 +0300

    Added hook for inputs, that datepicker applied, visualisation and removing form`s and field`s errors

--- jquery.formset.js
+++ jquery.formset.js
@@ -1,5 +1,5 @@
 /**
- * jQuery Formset 1.3-pre
+ * jQuery Formset 1.6-pre
  * @author Stanislaus Madueke (stan DOT madueke AT gmail DOT com)
  * @requires jQuery 1.2.6 or later
  *
@@ -27,13 +27,28 @@
                 }
             },
 
+            removeClasses = function(row) {
+                if (options.classesForRemove) {
+                    row.find('.' + options.classesForRemove.join(', .')).each(function() {
+                        $(this).removeClass(options.classesForRemove.join(' '));
+                    });
+                }
+            },
+
+            cleanErrors = function(row) {
+                if (options.formErrorContainersClasses) {
+                    row.find('.' + options.formErrorContainersClasses.join(', .')).each(function() {
+                        $(this).html('');
+                    });
+                }
+            },
+
             updateElementIndex = function(elem, prefix, ndx) {
                 var idRegex = new RegExp(prefix + '-(\\d+|__prefix__)-'),
                     replacement = prefix + '-' + ndx + '-';
                 if (elem.attr("for")) elem.attr("for", elem.attr("for").replace(idRegex, replacement));
                 if (elem.attr('id')) elem.attr('id', elem.attr('id').replace(idRegex, replacement));
                 if (elem.attr('name')) elem.attr('name', elem.attr('name').replace(idRegex, replacement));
-
             },
 
             hasChildElements = function(row) {
@@ -56,65 +71,44 @@
             insertDeleteLink = function(row) {
                 var delCssSelector = $.trim(options.deleteCssClass).replace(/\s+/g, '.'),
                     addCssSelector = $.trim(options.addCssClass).replace(/\s+/g, '.');
-                if (row.is('TR')) {
+
+                var delButtonHTML = '<a class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a>';
+                if (options.deleteContainerClass) {
+                    // If we have a specific container for the remove button,
+                    // place it as the last child of that container:
+                    row.find('[class*="' + options.deleteContainerClass + '"]').append(delButtonHTML);
+                } else if (row.is('TR')) {
                     // If the forms are laid out in table rows, insert
                     // the remove button into the last table cell:
-                    row.children(':last').append('<a id="delete_'+row.rowIndex+'" class="' + options.deleteCssClass +'" href="javascript:void(0)">' + options.deleteText + '</a>');
+                    row.children(':last').append(delButtonHTML);
                 } else if (row.is('UL') || row.is('OL')) {
                     // If they're laid out as an ordered/unordered list,
                     // insert an <li> after the last list item:
-                    row.append('<li><a id="delete_'+row.rowIndex+'" class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a></li>');
+                    row.append('<li>' + delButtonHTML + '</li>');
                 } else {
                     // Otherwise, just insert the remove button as the
                     // last child element of the form's container:
-                    row.append('<a id="delete_'+row.rowIndex+'" class="' + options.deleteCssClass + '" href="javascript:void(0)">' + options.deleteText +'</a>');
+                    row.append(delButtonHTML);
                 }
+
                 // Check if we're under the minimum number of forms - not to display delete link at rendering
                 if (!showDeleteLinks()){
                     row.find('a.' + delCssSelector).hide();
                 }
+
                 row.find('a.' + delCssSelector).click(function() {
                     var row = $(this).parents('.' + options.formCssClass),
                         del = row.find('input:hidden[id $= "-DELETE"]'),
                         buttonRow = row.siblings("a." + addCssSelector + ', .' + options.formCssClass + '-add'),
                         forms;
-                    
-                    // if (del.length) {
-                    //     // We're dealing with an inline formset.
-                    //     // Rather than remove this form from the DOM, we'll mark it as deleted
-                    //     // and hide it, then let Django handle the deleting:
-                    //     del.val('on');
-                    //     row.remove();
-                    //     // row.hide();
-                    //     forms = $('.' + options.formCssClass).not(':hidden');
-                    //      totalForms.val(forms.length)
-                    // } else {
-                    //     row.remove();
-                    //     // Update the TOTAL_FORMS count:
-                    //     forms = $('.' + options.formCssClass).not('.formset-custom-template');
-                    //     totalForms.val(forms.length);
-                    // }
-                    // for (var i=0, formCount=forms.length; i<formCount; i++) {
-                    //     // Apply `extraClasses` to form rows so they're nicely alternating:
-                    //     applyExtraClasses(forms.eq(i), i);
-                    //     if (!del.length) {
-                    //         // Also update names and IDs for all child controls (if this isn't
-                    //         // a delete-able inline formset) so they remain in sequence:
-                    //         forms.eq(i).find(childElementSelector).each(function() {
-                    //             updateElementIndex($(this), options.prefix, i);
-                    //         });
-                    //     }
-                    // }
                     if (del.length) {
                         // We're dealing with an inline formset.
                         // Rather than remove this form from the DOM, we'll mark it as deleted
                         // and hide it, then let Django handle the deleting:
                         del.val('on');
-                        //row.remove();
                         row.hide();
-                        //forms = $('.' + options.formCssClass).not(':hidden');
-                        forms = $('.' + options.formCssClass);
-                         totalForms.val(forms.length)
+                        forms = $('.' + options.formCssClass).not(':hidden');
+                        // totalForms.val(forms.length);
                     } else {
                         row.remove();
                         // Update the TOTAL_FORMS count:
@@ -124,15 +118,14 @@
                     for (var i=0, formCount=forms.length; i<formCount; i++) {
                         // Apply `extraClasses` to form rows so they're nicely alternating:
                         applyExtraClasses(forms.eq(i), i);
-                        
-                        // Also update names and IDs for all child controls (if this isn't
-                        // a delete-able inline formset) so they remain in sequence:
-                        forms.eq(i).find(childElementSelector).each(function() {
-                            updateElementIndex($(this), options.prefix, i);
-                        });
-                        
+                        if (!del.length) {
+                            // Also update names and IDs for all child controls (if this isn't
+                            // a delete-able inline formset) so they remain in sequence:
+                            forms.eq(i).find(childElementSelector).each(function() {
+                                updateElementIndex($(this), options.prefix, i);
+                            });
+                        }
                     }
-
                     // Check if we've reached the minimum number of forms - hide all delete link(s)
                     if (!showDeleteLinks()){
                         $('a.' + delCssSelector).each(function(){$(this).hide();});
@@ -187,6 +180,7 @@
             } else {
                 // Otherwise, use the last form in the formset; this works much better if you've got
                 // extra (>= 1) forms (thnaks to justhamade for pointing this out):
+                if (options.hideLastAddForm) $('.' + options.formCssClass + ':last').hide();
                 template = $('.' + options.formCssClass + ':last').clone(true).removeAttr('id');
                 template.find('input:hidden[id $= "-DELETE"]').remove();
                 // Clear all cloned fields, except those the user wants to keep (thanks to brunogola for the suggestion):
@@ -204,32 +198,51 @@
             // FIXME: Perhaps using $.data would be a better idea?
             options.formTemplate = template;
 
-            if ($$.is('TR')) {
+            var addButtonHTML = '<a class="' + options.addCssClass + '" href="javascript:void(0)">' + options.addText + '</a>';
+            if (options.addContainerClass) {
+                // If we have a specific container for the "add" button,
+                // place it as the last child of that container:
+                var addContainer = $('[class*="' + options.addContainerClass + '"');
+                addContainer.append(addButtonHTML);
+                addButton = addContainer.find('[class="' + options.addCssClass + '"]');
+            } else if ($$.is('TR')) {
                 // If forms are laid out as table rows, insert the
                 // "add" button in a new table row:
                 var numCols = $$.eq(0).children().length,   // This is a bit of an assumption :|
-                    buttonRow = $('<tr><td colspan="' + numCols + '"><a id="addRow" class="' + options.addCssClass + '" href="javascript:void(0)">' + options.addText + '</a></tr>')
-                                .addClass(options.formCssClass + '-add');
+                    buttonRow = $('<tr><td colspan="' + numCols + '">' + addButtonHTML + '</tr>').addClass(options.formCssClass + '-add');
                 $$.parent().append(buttonRow);
-                if (hideAddButton) buttonRow.hide();
                 addButton = buttonRow.find('a');
             } else {
                 // Otherwise, insert it immediately after the last form:
-                $$.filter(':last').after('<a id="addRow" class="' + options.addCssClass + '" href="javascript:void(0)">' + options.addText + '</a>');
+                $$.filter(':last').after(addButtonHTML);
                 addButton = $$.filter(':last').next();
-                if (hideAddButton) addButton.hide();
             }
+
+            if (hideAddButton) addButton.hide();
+
             addButton.click(function() {
                 var formCount = parseInt(totalForms.val()),
-                    // row = options.formTemplate.clone(true).removeClass('formset-custom-template'),
                     row = options.formTemplate.clone(true).removeClass('formset-custom-template'),
                     buttonRow = $($(this).parents('tr.' + options.formCssClass + '-add').get(0) || this),
+                    formSetContainer = $('.' + options.formSetContainerClass),
                     delCssSelector = $.trim(options.deleteCssClass).replace(/\s+/g, '.');
                 applyExtraClasses(row, formCount);
-                row.insertBefore(buttonRow).show();
+                if (formSetContainer) {
+                    row.appendTo(formSetContainer).show();
+                } else {
+                    row.insertBefore(buttonRow).show();
+                }
                 row.find(childElementSelector).each(function() {
                     updateElementIndex($(this), options.prefix, formCount);
                 });
+                cleanErrors(row);
+                removeClasses(row);
+                // datepicker-s hook
+                if (options.datepickerClass) {
+                    row.find('.' + options.datepickerClass).each(function() {
+                        $(this).datepicker('destroy').datepicker();
+                    });
+                }
                 totalForms.val(formCount + 1);
                 // Check if we're above the minimum allowed number of forms -> show all delete link(s)
                 if (showDeleteLinks()){
@@ -252,12 +265,19 @@
         formTemplate: null,              // The jQuery selection cloned to generate new form instances
         addText: 'add another',          // Text for the add link
         deleteText: 'remove',            // Text for the delete link
+        addContainerClass: null,         // Container CSS class for the add link
+        deleteContainerClass: null,      // Container CSS class for the delete link
         addCssClass: 'add-row',          // CSS class applied to the add link
         deleteCssClass: 'delete-row',    // CSS class applied to the delete link
         formCssClass: 'dynamic-form',    // CSS class applied to each form in a formset
         extraClasses: [],                // Additional CSS classes, which will be applied to each form in turn
+        classesForRemove: [],            // Additional CSS classes, which will be removed in new added form
         keepFieldValues: '',             // jQuery selector for fields whose values should be kept when the form is cloned
         added: null,                     // Function called each time a new form is added
-        removed: null                    // Function called each time a form is deleted
+        removed: null,                   // Function called each time a form is deleted
+        hideLastAddForm: false,          // When set to true, hide last empty add form (becomes visible when clicking on add button)
+        formSetContainerClass: null,     // Container CSS class for FormSet
+        formErrorContainersClasses: [],  // Container CSS class for Form errors
+        datepickerClass: null            // CSS class for inputs, that Datepicker widget applied
     };
 })(jQuery);
